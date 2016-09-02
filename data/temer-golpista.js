79aca25208f1ff0bb78a09790a3efbd1041e2b7a
Marina Limeira <marinaflessa@gmail.com>
2016-09-02 02:54:39 -0300

    Add listener to replace content

---
+++ data/temer-golpista.js
@@ -0,0 +1,3 @@
+window.addEventListener("DOMContentLoaded", function() {
+    document.body.innerHTML = document.body.innerHTML.replace(/Michel Temer/g, "Temer Golpista");
+});
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var self = require('sdk/self');
+var pageMod = require("sdk/page-mod");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
-}
+pageMod.PageMod({
+  include: "*",
+  contentScriptFile: "./temer-golpista.js",
+  contentScriptWhen: "ready"
+});
 
-exports.dummy = dummy;
